94da66a562d6e518def58c92d02ce286a6864f0b
xVanAn1612 <Anhvse172857@fpt.edu.vn>
2025-02-21 00:11:28 +0700

    roi a

--- FE/src/components/Admin/AdminLayout.jsx
+++ FE/src/components/Admin/AdminLayout.jsx
@@ -26,7 +26,7 @@ const AdminLayout = () => {
       <div
         className={`bg-gray-900 w-64 min-h-screen text-white absolute md:relative transform ${
           isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-        } transition-transform duration-300 md `}
+        } transition-transform duration-300 md:p `}
       ></div>
     </div>
   );
